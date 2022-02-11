05d7a708cd1907841ddd14482645f4f785d380c8
avrcoelho <andrevrcoelho@hotmail.com>
2022-02-10 21:16:56 -0300

    feat: strore

---
+++ src/presentation/store/NotificationStore.ts
@@ -0,0 +1,44 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import { NotificationProps } from '../types/Notification';
+
+export type NotificationData = Omit<NotificationProps, 'onRemove' | 'amount'>;
+
+export class NotificationStore {
+  private static instance: NotificationStore;
+
+  private notifications: NotificationData[];
+
+  private setState: Dispatch<SetStateAction<NotificationData[]>> | undefined;
+
+  private constructor() {
+    this.notifications = [];
+  }
+
+  static getInstance(): NotificationStore {
+    if (!NotificationStore.instance) {
+      NotificationStore.instance = new NotificationStore();
+    }
+    return NotificationStore.instance;
+  }
+
+  subscribe(setState: Dispatch<SetStateAction<NotificationData[]>>): void {
+    this.setState = setState;
+  }
+
+  add(value: NotificationData): void {
+    this.notifications = [...this.notifications, value];
+    this.setState?.(this.notifications);
+  }
+
+  remove(id: string): void {
+    this.notifications = this.notifications.filter(
+      notification => notification.id !== id,
+    );
+    this.setState?.(this.notifications);
+  }
+
+  get(): NotificationData[] {
+    return this.notifications;
+  }
+}
