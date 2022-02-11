1bed128318833db0bf52238264dc585aa4d78a8e
avrcoelho <andrevrcoelho@hotmail.com>
2022-02-10 21:17:10 -0300

    test: store

---
+++ src/presentation/store/__tests__/Notification.spec.ts
@@ -0,0 +1,30 @@
+import uuid from 'react-native-uuid';
+
+import { NotificationType } from '../../types/Notification';
+import { NotificationStore } from '../NotificationStore';
+
+describe('NotificationStore', () => {
+  const notificationData = {
+    id: String(uuid.v4()),
+    type: 'success' as NotificationType,
+    text: 'test',
+  };
+  it('should be able to add notification', () => {
+    const notificationStore = NotificationStore.getInstance();
+    notificationStore.subscribe(jest.fn());
+
+    notificationStore.add(notificationData);
+
+    expect(notificationStore.get()).toHaveLength(1);
+  });
+
+  it('should be able to remove notification', () => {
+    const notificationStore = NotificationStore.getInstance();
+    notificationStore.subscribe(jest.fn());
+
+    notificationStore.add(notificationData);
+    notificationStore.remove(notificationData.id);
+
+    expect(notificationStore.get()).toHaveLength(0);
+  });
+});
