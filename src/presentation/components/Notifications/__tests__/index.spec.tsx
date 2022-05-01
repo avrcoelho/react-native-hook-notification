255b54b29c557becdec9fca9858afc92de5a2639
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-01 14:32:37 -0300

    test: on another componentes

--- src/presentation/components/Notifications/__tests__/index.spec.tsx
+++ src/presentation/components/Notifications/__tests__/index.spec.tsx
@@ -3,15 +3,27 @@ import { render } from '@testing-library/react-native';
 
 import { Notifications } from '..';
 
+const mockUseControllerReturn = {
+  onRemove: jest.fn(),
+  notifications: [
+    {
+      text: 'test',
+    },
+  ],
+};
 jest.mock('../useController', () => ({
-  useController: () => ({
-    onRemove: jest.fn(),
-    notifications: [],
-  }),
+  useController: () => mockUseControllerReturn,
 }));
 
 describe('Notifications component', () => {
-  it('should be able to render component', () => {
+  it('should be able to render component whithout errors', () => {
+    expect(() => {
+      render(<Notifications />);
+    }).not.toThrow();
+  });
+
+  it('should not be able to render component whithout errors', () => {
+    mockUseControllerReturn.notifications = [];
     expect(() => {
       render(<Notifications />);
     }).not.toThrow();
