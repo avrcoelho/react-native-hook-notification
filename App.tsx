491e5c561af24726d825bc504922234ef65601eb
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-01 10:49:51 -0300

    test: useOrientation hook

--- App.tsx
+++ App.tsx
@@ -24,7 +24,7 @@ const App = (): JSX.Element => {
       text: 'My first test! My first test! My first test! My first test!',
       position: 'bottom-left',
       dragDirection: 'y',
-      transition: 'bounce',
+      transition: 'zoom',
     });
   };
 
---
+++ src/presentation/hooks/__tests__/useOrientation.spec.ts
@@ -0,0 +1,30 @@
+import { renderHook } from '@testing-library/react-hooks';
+import RN from 'react-native';
+
+import { useOrientation } from '../useOrientation';
+
+describe('useOrientation hook', () => {
+  it('should be able to return landscape orientation', () => {
+    jest.spyOn(RN, 'useWindowDimensions').mockReturnValueOnce({
+      width: 100,
+      height: 50,
+      fontScale: 1,
+      scale: 1,
+    });
+    const { result } = renderHook(() => useOrientation());
+
+    expect(result.current).toBe('landscape');
+  });
+
+  it('should be able to return portrait orientation', () => {
+    jest.spyOn(RN, 'useWindowDimensions').mockReturnValueOnce({
+      width: 100,
+      height: 150,
+      fontScale: 1,
+      scale: 1,
+    });
+    const { result } = renderHook(() => useOrientation());
+
+    expect(result.current).toBe('portrait');
+  });
+});
