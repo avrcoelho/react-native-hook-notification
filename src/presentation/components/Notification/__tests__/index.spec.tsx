9f71ed812becb8917170c3d87c3d70bb81f3ff6d
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-01 14:26:26 -0300

    test: notification ui

---
+++ src/presentation/components/Notification/__tests__/index.spec.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+
+import { NotificationType } from '../../../types/Notification';
+import { Notification } from '..';
+
+const mockOnRemoveNotification = jest.fn();
+jest.mock('../useController', () => ({
+  useController: () => ({
+    animatedStyle: {},
+    onGestureEvent: jest.fn(),
+    typeAndTheme: 'defaultcolored',
+    animation: {
+      enter: {
+        withCallback: jest.fn(),
+      },
+      exit: jest.fn(),
+    },
+    isPaused: false,
+    onFinishAnimation: jest.fn(),
+    onRemoveNotification: mockOnRemoveNotification,
+  }),
+}));
+
+const mockOnRemove = jest.fn();
+const props = {
+  type: 'success' as NotificationType,
+  id: '124',
+  text: 'text test',
+  amount: 1,
+  onRemove: mockOnRemove,
+  draggable: true,
+};
+describe('Notification component', () => {
+  it('should be able to render remove on click Notification', () => {
+    const { getByA11yLabel } = render(<Notification {...props} />);
+
+    fireEvent.press(getByA11yLabel('Close notification'));
+
+    expect(mockOnRemoveNotification).toBeCalled();
+  });
+
+  it('should not be able to render button close', () => {
+    Object.assign(props, {
+      showButtonClose: false,
+    });
+    const { queryByA11yLabel } = render(<Notification {...props} />);
+
+    expect(queryByA11yLabel('Close notification')).toBeFalsy();
+  });
+
+  it('should be able to render title', () => {
+    Object.assign(props, {
+      title: 'title test',
+      draggable: undefined,
+    });
+    const { getByText } = render(<Notification {...props} />);
+
+    expect(getByText('title test')).toBeTruthy();
+  });
+});
