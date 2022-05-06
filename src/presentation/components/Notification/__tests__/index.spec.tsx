6e04f74811f32bf411f67c486a92e966c9a421ea
avrcoelho <andrevrcoelho@hotmail.com>
2022-05-06 13:28:42 -0300

    test: notification ui test

--- src/presentation/components/Notification/__tests__/index.spec.tsx
+++ src/presentation/components/Notification/__tests__/index.spec.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { render, fireEvent } from '@testing-library/react-native';
 
 import { NotificationType } from '../../../types/Notification';
 import { Notification } from '..';
+import { Colors } from '../../../constants/Colors';
 
 let mockIsPortrait = false;
+let mockTypeAndTheme = 'defaultcolored';
 jest.mock('../useController', () => ({
   useController: () => ({
     animatedStyle: {},
     onGestureEvent: jest.fn(),
-    typeAndTheme: 'defaultcolored',
+    typeAndTheme: mockTypeAndTheme,
     animation: {
       enter: {
         withCallback: jest.fn(),
@@ -35,6 +37,12 @@ const props = {
   icon: <View />,
 };
 describe('Notification component', () => {
+  it('should not be able to render button close', () => {
+    const { queryByA11yLabel } = render(<Notification {...props} />);
+
+    expect(queryByA11yLabel('Close notification')).toBeFalsy();
+  });
+
   it('should be able to render remove on click Notification', () => {
     Object.assign(props, {
       showButtonClose: true,
@@ -46,15 +54,6 @@ describe('Notification component', () => {
     expect(props.onRemove).toBeCalled();
   });
 
-  it('should not be able to render button close', () => {
-    Object.assign(props, {
-      showButtonClose: false,
-    });
-    const { queryByA11yLabel } = render(<Notification {...props} />);
-
-    expect(queryByA11yLabel('Close notification')).toBeFalsy();
-  });
-
   it('should be able to render title', () => {
     Object.assign(props, {
       title: 'title test',
@@ -64,5 +63,54 @@ describe('Notification component', () => {
     const { getByText } = render(<Notification {...props} />);
 
     expect(getByText('title test')).toBeTruthy();
+  });
+
+  it('should be able to render custom icon', () => {
+    const Icon = (): JSX.Element => <Text>IC</Text>;
+    Object.assign(props, {
+      icon: <Icon />,
+      showButtonClose: true,
+    });
+    mockIsPortrait = true;
+    const { getByText } = render(<Notification {...props} />);
+
+    expect(getByText('IC')).toBeTruthy();
+  });
+
+  it('should be able to render custom notification with custom styles', () => {
+    mockTypeAndTheme = 'defaultcustom';
+    Object.assign(props, {
+      type: 'custom',
+      theme: 'custom',
+      showButtonClose: true,
+      customStyle: {
+        container: {
+          backgroundColor: Colors.Black,
+        },
+        title: {
+          backgroundColor: Colors.Black,
+        },
+        text: {
+          backgroundColor: Colors.Black,
+        },
+        icon: {
+          backgroundColor: Colors.Black,
+        },
+        button: {
+          backgroundColor: Colors.Black,
+        },
+        buttonText: {
+          backgroundColor: Colors.Black,
+        },
+      },
+    });
+    mockIsPortrait = true;
+    const { getByText } = render(<Notification {...props} />);
+
+    expect(getByText('title test').props.style[1]).toEqual(
+      expect.objectContaining({
+        backgroundColor: Colors.Black,
+      }),
+    );
   });
 });
